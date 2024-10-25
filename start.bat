dcc9bad1470c4415bd503a441a39d59453d0925e
CameronH64 <cholbrook582@gmail.com>
2024-10-25 12:41:33 -0500

    Add Script to Run the Python.

---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off
+
+:: Run the Python script
+C:\Users\gold1\pycharm-projects\youtube-audio-transcriber\.venv\Scripts\python.exe youtube_audio_transcriber.py
+
+:: Pause the console (optional, if you want the window to remain open after execution)
+pause
