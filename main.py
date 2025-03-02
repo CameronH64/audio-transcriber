e92f8ec18cffa019097b48071816eafb9a413bb4
CameronH64 <cholbrook582@gmail.com>
2025-03-02 10:10:34 -0600

    Minor Refactors, Update requirements.txt.

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def paragraphize_text(input_string):
     return result_string
 
 
-def transcribe_audio(input_audio_file: str, cuda_device):
+def transcribe_audio(input_audio_file, cuda_device):
 
     print(f'Running audio conversion for: {os.path.basename(input_audio_file)}')
 
@@ -49,7 +49,7 @@ def transcribe_audio(input_audio_file: str, cuda_device):
 
 def transcribe_and_output_text(audio_file_folder, audio_file, cuda_device, transcription_output_folder):
 
-    transcribed_text = transcribe_audio(os.path.join(audio_file_folder, audio_file), cuda_device)
+    transcribed_text = transcribe_audio(str(os.path.join(audio_file_folder, audio_file)), cuda_device)
     # After doing the transcription, make the .txt file with the text transcription.
     create_transcription_file(transcribed_text, audio_file, transcription_output_folder)
 
@@ -59,26 +59,40 @@ def read_settings_file():
     with open("settings.json", "r") as f:
         settings_json = json.load(f)
 
-    return settings_json
-
-
-def main():
-
-    # Check if CUDA is available
-    cuda_device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    settings_json = read_settings_file()
-
     audio_file_folder = settings_json['input_audio']
     transcription_output_folder = settings_json['output_transcriptions']
     log_file = settings_json['log_file']
 
-    # Check if the log file exists and is not empty so that the headers aren't written again.
+    return audio_file_folder, transcription_output_folder, log_file
+
+
+def generate_time(start_time):
+
+    end_time = time.time()  # End timing
+    elapsed_time = end_time - start_time  # Calculate elapsed time
+    # Convert seconds to mm:ss format
+    minutes = int(elapsed_time // 60)
+    seconds = int(elapsed_time % 60)
+    return minutes, seconds
+
+
+
+def main():
+
+    # ----- Check if CUDA is available
+    cuda_device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # ----- Extract the JSON settings -----
+
+    audio_file_folder, transcription_output_folder, log_file = read_settings_file()
+
+    # ----- Check if the log file exists and is not empty so that the headers aren't written again -----
     file_exists = os.path.isfile(log_file) and os.path.getsize(log_file) > 0
 
+    # ----- Write the log file -----
     with open(log_file, "a", newline="") as file:
 
-        writer = csv.writer(file)
+        writer = csv.writer(file)       # Load the .csv file into memory.
 
         # Write headers only if the file does not exist or is empty
         if not file_exists:
@@ -91,12 +105,7 @@ def main():
 
             transcribe_and_output_text(audio_file_folder, audio_file, cuda_device, transcription_output_folder)
 
-            end_time = time.time()  # End timing
-            elapsed_time = end_time - start_time  # Calculate elapsed time
-
-            # Convert seconds to mm:ss format
-            minutes = int(elapsed_time // 60)
-            seconds = int(elapsed_time % 60)
+            minutes, seconds = generate_time(start_time)
 
             processing_message = f"Time for processing '{audio_file}': {minutes:02}:{seconds:02}"
             processing_message_list = [f"{minutes:02}:{seconds:02}", f"{audio_file}"]
@@ -104,6 +113,7 @@ def main():
             print(processing_message + "\n")
 
             writer.writerow(processing_message_list)
+
 
 
 if __name__ == '__main__':
